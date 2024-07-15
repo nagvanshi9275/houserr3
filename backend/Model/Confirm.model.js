51553f7dc86c2fe39161b24892231b588bab92b8
Ashish Nagvanshi <aashishsingh24032005@gmail.com>
2024-07-15 12:51:27 +0530

    created another schema for  the final confirmed user purches data

---
+++ backend/Model/Confirm.model.js
@@ -0,0 +1,72 @@
+
+
+import mongoose from "mongoose";
+
+
+
+
+const userSchema = new mongoose.Schema({
+
+    city:{
+     
+     type: String,
+     required: true
+     
+
+
+
+    },
+
+    location:{
+     type: String,
+     required: true
+
+
+    },
+
+    pricing:{
+     type: Number,
+     
+     required: true
+
+
+
+
+    },
+
+    name:{
+     type: String,
+
+     required: true
+
+
+
+
+    },
+
+    email:{
+    type: String,
+    required: true
+
+    },
+
+    phone:{
+
+    type: Number,
+    required: true
+
+    }
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
