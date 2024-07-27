32ee6a534d68fb2efd3da4e25cf6e6ebeae0672b
Ashish Nagvanshi <aashishsingh24032005@gmail.com>
2024-07-27 19:59:47 +0530

    fixed the bug on .env config

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import 'dotenv/config';
 import router from './Routes/user.routes.js';
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(cors({
   origin: '*'
