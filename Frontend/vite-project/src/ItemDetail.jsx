d68aafadb61cc355579f85d53ecb3100f091aa50
n61628653 <n61628653@gmail.com>
2024-09-16 06:28:15 +0000

    changes find space section

--- Frontend/vite-project/src/ItemDetail.jsx
+++ Frontend/vite-project/src/ItemDetail.jsx
@@ -32,7 +32,7 @@ export default function ItemDetail() {
   return (
     <Container>
 
-     <Spaceheader/>
+  
 
       <Typography variant="h4" component="h1" gutterBottom>
         {item.name}
@@ -79,6 +79,10 @@ export default function ItemDetail() {
           <Typography variant="body1">Location</Typography>
         </Grid>
       </Grid>
+
+
+      <Spaceheader/>
+
     </Container>
   );
 }
